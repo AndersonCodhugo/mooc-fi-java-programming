c2b3a1302f7faa65857c18e3fc6abab6d0d2c2e7
AndersonCodhugo <anderson_hindo@hotmail.com>
2025-08-26 08:53:35 -0300

    Introdution POO

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -5,6 +5,14 @@ public class YourFirstBankTransfer {
         // Do not touch the code in Account.java
         // write your program here
         
+        Account mattAccount = new Account("Matthews account", 1000.0);
         
+        Account myAccount = new Account("My account", 0.0);
+        
+        mattAccount.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        
+        System.out.println(mattAccount);
+        System.out.println(myAccount);
     }
 }
