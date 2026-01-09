2e10f1be93138718f5203c4377c620c7ffcc760c
AndersonCodhugo <anderson_hindo@hotmail.com>
2026-01-09 10:20:52 -0300

    75% of objects and references

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,34 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared) {
+        if (squares > compared.squares ) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int priceDifference(Apartment compared) {
+        int differenceValue = (pricePerSquare * squares) - (compared.pricePerSquare * compared.squares);
+        if (differenceValue < 0) {
+            differenceValue = differenceValue * -1;
+        }
+        
+        return differenceValue; 
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        if ((pricePerSquare * squares) > (compared.pricePerSquare * compared.squares)) {
+            return true;
+        }
+        return false;
+    }
 }
